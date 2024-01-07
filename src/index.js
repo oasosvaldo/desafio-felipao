0543e9d1f6a3138bc30cdee84481d0b751768d3e
oasosvaldo <oasosvaldo@gmail.com>
2024-01-07 19:41:02 -0300

    Desenvolvendo codigo e add imagens

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 function verificar() {
-  var nomeHeroi = document.getElementById('txtNome')
-  var xp = document.getElementById('xp')
+  let nomeHeroi = document.getElementById('txtNome')
+  let xp = document.getElementById('xp')
+  let res = document.getElementById('res')
 
   if (nomeHeroi.value == 0 && xp.value == 0) {
       alert('[ERRO] Verifique os dados e tente novamente!')
   } else {
-    res.innerHTML = "Que Heroi"
+    let img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (nomeHeroi.value === "Thor" && xp.value < 1000) {
+      res.innerHTML = "Ferro"
+    }
   }
 }
 
