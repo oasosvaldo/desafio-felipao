90e0210330f3f33da62d8130e1064718337a8999
oasosvaldo <oasosvaldo@gmail.com>
2024-01-07 19:18:40 -0300

    Testando function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function verificar() {
-  var nomeHeroi = "teste1"
-  var xp = 1000
+  var nomeHeroi = document.getElementById('txtNome')
+  var xp = document.getElementById('xp')
 
   if (nomeHeroi.value == 0 && xp.value == 0) {
       alert('[ERRO] Verifique os dados e tente novamente!')
